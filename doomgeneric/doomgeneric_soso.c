0f488ee086caa372135c329b41b2e942ab51503b
ozkl <ozkl@users.noreply.github.com>
2020-12-12 18:00:29 +0300

    clang istead of gcc

--- doomgeneric/doomgeneric_soso.c
+++ doomgeneric/doomgeneric_soso.c
@@ -115,8 +115,7 @@ void enableRawMode()
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
-  //raw.c_lflag &= ~(ECHO);
+  raw.c_lflag &= ~(ECHO);
   raw.c_cc[VMIN] = 0;
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
